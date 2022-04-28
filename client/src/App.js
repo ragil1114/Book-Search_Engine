db39bf947c810fb53b569d0cb7ad5cd98241a7c9
Ragil14 <rahil.gilani@gmail.com>
2022-04-28 12:43:42 -0500

    add http & auth Links & ApolloProvider to verify user

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// Create an http link to the local server
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Create an authlink to verify the user
+const authlink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// Create an instance of the ApolloClient
+const client = new ApolloClient({
+  link: authlink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +41,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
